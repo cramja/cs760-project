35eb4799e5abd370a356d1b5a997f60486ef34e8
cramja <marc.spehlmann@gmail.com>
2017-03-20 13:23:54 -0500

    added bias term to svm

--- vis/create_seperable_data.m
+++ vis/create_seperable_data.m
@@ -2,13 +2,13 @@ function [ x ] = create_seperable_data()
 %CREATE_SEPERABLE_DATA
 %
 %   returns matrix of the form [[x0,x1,y], [x0,x1,y], ... ]
-%
+%   centers one dataset around 0,0 and the other around 3,3
     examples = 50;
     ex2 = examples/2;
     pd = makedist('Normal');
-    x = [pd.random(examples, 2), ones(examples, 1)];
+    x = [pd.random(examples, 2) - 0.5, ones(examples, 1)];
     x(1:ex2, 1:2) = x(1:examples/2, 1:2) + 1;
-    x(ex2:examples, 1:2) = x(ex2:examples, 1:2) - 1;
+    x(ex2:examples, 1:2) = x(ex2:examples, 1:2) + 3;
     x(ex2:examples, 3) = -1;
 end
 
--- vis/svm.m
+++ vis/svm.m
@@ -1,46 +1,55 @@
-% SVM with l2 regularizer
+% L1 SVM with L2 regularizer
 
-x = create_seperable_data()
+x = create_seperable_data();
+x = [ones(size(x, 1), 1) x]; % add bias term
+y = x(:, 4);
+x = x(:, 1:3);
 
 clf;
 fig = figure(1);
-hold on
-set(fig, 'Position', [100 100 1000 800])
-sz = size(x)
-scatter(x(1:sz(1)/2,1),x(1:sz(1)/2,2), 'r')
-scatter(x(sz(1)/2:end,1),x(sz(1)/2:end,2), 'b')
+hold on;
+set(fig, 'Position', [100 100 1000 800]);
+sz = size(x);
+scatter(x(1:sz(1)/2,2),x(1:sz(1)/2,3), 'r');
+scatter(x(sz(1)/2:end,2),x(sz(1)/2:end,3), 'b');
 xlabel('x_1');
 ylabel('x_2');
 xlim([-5 5]);
 ylim([-5 5]);
 
-epochs = 50;
-learning_rate = 0.01;
-learning_rate_decay = 0.9;
-lambda = 0.2;
+epochs = 100;
+learning_rate = 0.05;
+learning_rate_decay = 0.95;
+lambda = 1;
 
-w = (rand(1,2) - 0.5) * 4;
+w = (rand(1,sz(2)) - 0.5) * 4;
 w_h = [w;w];
-for e = 1:epochs
-    x = x(randperm(sz(1)), :);
-    for i = 1:sz(1)
-        j = (x(i,1:2) * w') .* x(i,3);
+for e = 1:epochs;
+    u = 0;
+    for i = randperm(sz(1));
+        j = (x(i, :) * w') .* y(i) ;%.* -1;
         if j < 1
-            w = w - learning_rate .* (x(i,1:2) .* x(i,3)) - (w .* lambda .* inv(sz(1)));
-            w_h = [w_h; w];
+            u = u + 1;
+            w = w - (learning_rate .* (x(i,:) .* y(i) .* -1 + (w .* lambda .* inv(sz(1)))));
+        else % hinge
+            w = w - (learning_rate .* (w .* lambda .* inv(sz(1))));
         end
+        w_h = [w_h; w];
+        
         if exist('w_scatter')
             delete(w_scatter);
         end
         if exist('w_boundry')
             delete(w_boundry);
         end
-        a1 = [-4:0.25:4];
-        a2 = (-1 .* w(1) .* a1) ./  w(2);
-        w_boundry = plot(a1,a2,'r');
-        w_scatter = scatter(w_h(:,1), w_h(:,2), 'k.');
+        boundary_x = [-4:0.25:4];
+        boundary_y = (-1 .* ((w(2) .* boundary_x) + w(1))) ./  w(3);
+        w_boundry = plot(boundary_x, boundary_y,'r');
+        w_scatter = scatter(w_h(:,2), w_h(:,3), 'k.');
     end
+    u
     pause(0.05); % allows plot to update
-    accuracy = sum(sign(x(:,1:2) * w') ~= x(:,3)) / sz(1)
+    accuracy = sum(sign(x * w') == y) / sz(1);
+    disp([e accuracy]);
     learning_rate = learning_rate * learning_rate_decay;
 end
